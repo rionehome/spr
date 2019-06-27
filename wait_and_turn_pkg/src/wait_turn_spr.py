8ac5fbea6e66770f9715de9c985ba2d7438cd3b9
yamatomatsuda <yamato19990708@gmail.com>
2019-06-27 18:56:10 +0900

    回転が安定するようにした

--- wait_and_turn_pkg/src/wait_turn_spr.py
+++ wait_and_turn_pkg/src/wait_turn_spr.py
@@ -19,24 +19,31 @@ def NowAngular(message):
 def Turn_180():
 	global finish_turn
 	# print "A"
+	print "wait 10 second"
+	rospy.sleep(10)
 	pub_A1 = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
 	# print angular
-	vel = Twist()
-	vel.linear.x = 0.0
-	if angular > - 179.5 and angular < 177:
-		# print "1"
-		vel.angular.z = 0.6
-		pub_A1.publish(vel)
-	# print "1."
-	elif angular >= 177 and angular < 179.8:
+	while finish_turn is False:
+		vel = Twist()
+		vel.linear.x = 0.0
+		if angular > - 5 and angular < 177:
+			vel.angular.z = 0.6
+			pub_A1.publish(vel)
+		elif angular <=-5 and angular>=-177:
+			vel.angular.z=-0.6
+			pub_A1.publish(vel)
+		elif angular >= 177 and angular < 179.8:
 		# print "2"
-		vel.angular.z = 0.3
-		pub_A1.publish(vel)
-	# print "2."
-	else:
-		vel.angular.z = 0.0
-		pub_A1.publish(vel)
-		finish_turn = True
+			vel.angular.z = 0.3
+			pub_A1.publish(vel)
+
+		elif angular >-179.8 and angular<-177:
+			vel.angular.z=-0.3
+			pub_A1.publish(vel)
+		else:
+			vel.angular.z = 0.0
+			pub_A1.publish(vel)
+			finish_turn = True
 
 
 def listener():
@@ -51,14 +58,7 @@ def callback(data):
 	print "callback"
 	pub02 = rospy.Publisher('face_recognize', String, queue_size=10)
 	if data.data == '01':
-		print "wait 10 second"
-		rospy.sleep(10)
-		odom = Odometry()
-		while not rospy.is_shutdown():
-			Turn_180()
-			if finish_turn is True:
-				break
-
+		Turn_180()
 		print("Turn 180 degree.")
 		rospy.sleep(2)
 		rospy.loginfo(pub02)
